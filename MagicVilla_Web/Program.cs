ab34dbc414fe87b799044b6cf8cb65ac281e873d
nats2025 <natarajan.venkatesan@visionetsystems.com>
2024-11-29 08:08:50 +0000

    Update Program.cs

--- MagicVilla_Web/Program.cs
+++ MagicVilla_Web/Program.cs
@@ -7,7 +7,6 @@ using MagicVilla_Web.Extensions;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-//workflow test
 
 builder.Services.AddControllersWithViews(u=>u.Filters.Add(new AuthExceptionRedirection()));
 builder.Services.AddAutoMapper(typeof(MappingConfig));
